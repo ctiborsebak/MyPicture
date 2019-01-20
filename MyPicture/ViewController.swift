37e30d2bd361bf8b2fae3dc63a0c076d36251332
Ctibor Sebak <sebactibor@gmail.com>
2019-01-20 16:47:15 +0100

    Initial Commit

---
+++ MyPicture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPicture
+//
+//  Created by Ctibor Šebák on 20/01/2019.
+//  Copyright © 2019 Ctibor Šebák. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
