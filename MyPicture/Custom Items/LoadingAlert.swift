257338316f63000104b91ec9cfc5c474e8122ea4
Ctibor Sebak <sebactibor@gmail.com>
2019-01-21 20:31:20 +0100

    Base build -

---
+++ MyPicture/Custom Items/LoadingAlert.swift
@@ -0,0 +1,9 @@
+//
+//  LoadingAlert.swift
+//  MyPicture
+//
+//  Created by Ctibor Šebák on 21/01/2019.
+//  Copyright © 2019 Ctibor Šebák. All rights reserved.
+//
+
+import Foundation
---
+++ MyPicture/Extensions/UIColor+Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  UIColor+Extensions.swift
+//  MyPicture
+//
+//  Created by Ctibor Šebák on 20/01/2019.
+//  Copyright © 2019 Ctibor Šebák. All rights reserved.
+//
+
+import Foundation
